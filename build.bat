9db72e13c2f935f81d7c778729dfbca000dcce49
yagizdkurt <ydxolos@gmail.com>
2025-10-03 22:47:50 +0300

    1

--- build.bat
+++ build.bat
@@ -6,10 +6,10 @@ if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
 
 REM Add MinGW to PATH
-set PATH=C:\msys64\mingw64\bin;%PATH%
+set PATH=C:\msys64\ucrt64\bin;%PATH%
 
-REM Generate project with MinGW and SFML paths if needed
-cmake -S . -B %BUILD_DIR% -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH=outsrc/SFML
+REM Generate project with MinGW
+cmake -S . -B %BUILD_DIR% -G "MinGW Makefiles"
 
 REM Build
 cmake --build %BUILD_DIR% -- -j4
