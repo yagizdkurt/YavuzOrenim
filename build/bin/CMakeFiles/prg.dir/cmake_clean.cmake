26260aa28bb2db65a62e5e762d08c882df6e0e3f
yagizdkurt <ydxolos@gmail.com>
2025-10-03 23:22:38 +0300

    1

---
+++ build/bin/CMakeFiles/prg.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prg.dir/__/src/core.cpp.obj"
+  "CMakeFiles/prg.dir/__/src/core.cpp.obj.d"
+  "CMakeFiles/prg.dir/main.cpp.obj"
+  "CMakeFiles/prg.dir/main.cpp.obj.d"
+  "libprg.dll.a"
+  "prg.exe"
+  "prg.exe.manifest"
+  "prg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/bin/CMakeFiles/prg.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for prg.
+# This may be replaced when dependencies are built.
